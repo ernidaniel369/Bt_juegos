7ae6a24ac535a6728aa37cbdce9c558538202c18
ernidaniel369 <94999577+ernidaniel369@users.noreply.github.com>
2022-06-17 22:10:20 -0300

    se agrega proyecto

---
+++ server/src/controllers/indexController.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express';
+
+class IndexController{
+   public index (req: Request, res: Response) {
+        res.json({text: 'Api Is /api/games'});
+    } 
+}
+export const indexController = new IndexController();
