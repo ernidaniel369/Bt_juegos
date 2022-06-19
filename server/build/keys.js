1146ee4095035a785676afcb117634f0ab07d026
ernidaniel369 <94999577+ernidaniel369@users.noreply.github.com>
2022-06-19 18:22:04 -0300

    make it better

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'ng_games_db'
+        host: 'bw8mb6tzrlfi4mz4dfxo-mysql.services.clever-cloud.com',
+        user: 'u1doa91g7cglevgf',
+        password: '4U6Q1pVCMEoE4UMrB9dY',
+        database: 'bw8mb6tzrlfi4mz4dfxo'
     }
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,10 +1,10 @@
 export default {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'ng_games_db'
+        host: 'bw8mb6tzrlfi4mz4dfxo-mysql.services.clever-cloud.com',
+        user: 'u1doa91g7cglevgf',
+        password: '4U6Q1pVCMEoE4UMrB9dY',
+        database: 'bw8mb6tzrlfi4mz4dfxo'
     }
 
 }
